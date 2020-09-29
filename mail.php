2307d62816c40990499cefff8cecb166a8b70b6e
kiquino <kiquino@gmail.com>
2020-09-29 18:25:56 -0300

    Add files via upload

--- mail.php
+++ mail.php
@@ -7,5 +7,9 @@ $asunto = $_POST['asunto'];
 $mensaje = $_POST['mensaje'];
 
 $enviado = mail("kiquino@gmail.com",$asunto,$mensaje);
-
+if ($enviado) {
+    echo "mensaje enviado... <br>, <a href='index.html'>volver</a>";
+}else{
+    echo "ocurrio un error, <a href='contacto.html'>vuelva a intentarlo </a> y recuerda llenar todos los espacios";
+}
 ?>
